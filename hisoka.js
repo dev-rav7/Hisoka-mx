22271d6e5cf00ab00d43d59b1964c139adf8f89d
hhhisoka <enochkouame123@gmail.com>
2025-07-08 16:13:04 +0000

    update

--- hisoka.js
+++ hisoka.js
@@ -1,81 +1,31 @@
-var commands = [];
+const commands = [];
 
-function ven(commandConfig, actionFunction) {
-  var defaultConfig = {
-    desc: 'Not Provided',
-    category: 'misc'
-  };
-  
-  // Create command object from input config
-  var commandObject = commandConfig;
-  
-  // Set the action function
-  commandObject.action = actionFunction;
-  
-  // Set default values if not provided
-  if (!commandObject.dontAddCommandList) {
-    commandObject.dontAddCommandList = false;
-  }
-  
-  if (!commandObject.desc) {
-    commandObject.desc = '';
-  }
-  
-  if (!commandObject.fromMe) {
-    commandObject.fromMe = false;
-  }
-  
-  if (!commandObject.category) {
-    commandObject.category = defaultConfig.category;
-  }
-  
-  if (!commandObject.filename) {
-    commandObject.filename = defaultConfig.desc;
-  }
-  
-  // Add command to commands array
-  commands.push(commandObject);
-  
-  return commandObject;
+function ven(options, handler) {
+    if (typeof options === 'object' && typeof handler === 'function') {
+        // Command-style registration
+        const command = {
+            pattern: options.pattern || null,
+            alias: options.alias || [],
+            desc: options.desc || '',
+            category: options.category || 'misc',
+            react: options.react || null,
+            filename: options.filename || '',
+            use: options.use || '',
+            function: handler,
+            on: options.on || null
+        };
+        commands.push(command);
+    } else if (typeof options === 'object' && options.on) {
+        // Event-style registration (like "body", "text", etc.)
+        const event = {
+            on: options.on,
+            function: handler || options.function
+        };
+        commands.push(event);
+    }
 }
 
-// Export object with multiple aliases for the ven function
-var exportObject = {};
-exportObject.Function = ven;
-exportObject.AddCommand = ven;
-exportObject.Module = ven;
-exportObject.commands = commands;
-
-module.exports = exportObject;
-
-// Anti-debugging and obfuscation detection functions
-function antiDebug(counter) {
-  function debugCheck(input) {
-    if (typeof input !== 'string') {
-      return function(empty) {}
-        .constructor('while (true) {}')
-        .apply('counter');
-    } else {
-      if (('' + input / input).length !== 1 || input % 20 === 0) {
-        (function() {
-          return true;
-        }).constructor('debu' + 'gger').call('action');
-      } else {
-        (function() {
-          return false;
-        }).constructor('debu' + 'gger').apply('stateObject');
-      }
-    }
-    debugCheck(++input);
-  }
-  
-  try {
-    if (counter) {
-      return debugCheck;
-    } else {
-      debugCheck(0);
-    }
-  } catch (error) {
-    // Silent error handling
-  }
-}
+module.exports = {
+    ven,
+    commands
+};
